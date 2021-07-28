5a0c0675af1e5078b41b2352d2b0897aafe0e84d
Sarthak Garg <sarthakgargx@gmail.com>
2021-07-28 11:38:46 +0530

    Updated viewPhone

--- router/viewPhones.js
+++ router/viewPhones.js
@@ -5,14 +5,13 @@ const Phone = require("../model/PhoneSchema");
 
 
 router.get("/viewPhones", async (req, res) => {
-    const Phonedata = await Phone.find();
+  const Phonedata = await Phone.find();
   console.log(Phonedata)
   if (!Phonedata) {
     return res.json({ status: "Try Again", error: "Bhaiya Nahi Milega" });
   }
-  else res.json({ data:Phonedata})
-
-}
+  else res.json({ data: Phonedata })
+  }
 )
 
 module.exports = router;
